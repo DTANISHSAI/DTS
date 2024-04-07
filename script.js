d4a88a8586bf7edbc3d9e05322634e8752f6f86f
DTANISHSAI <149003494+DTANISHSAI@users.noreply.github.com>
2024-04-07 23:14:48 +0530

    change

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ console.log("New");
 	let  yearborn;
 	prompt("Enter the year of your date of birth: ");
 	if (age == 1972) {
-		console.log("Hi Nanna!");
-		console.log("My warm greetings to you Nanna.");
-		console.log("I am blessed to have a father like you Nanna.");
+		prompt("Hi Nanna!");
+		prompt("My warm greetings to you Nanna.");
+		prompt("I am blessed to have a father like you Nanna.");
 		
 
 	}
