192087646d824d5cfd97b61a0223ebbde55d9f17
DTANISHSAI <149003494+DTANISHSAI@users.noreply.github.com>
2024-04-07 23:08:33 +0530

    change

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("New");
+	let  yearborn;
+	prompt("Enter the year of your date of birth: ");
+	if (age == 1972) {
+		console.log("Hi Nanna!");
+		console.log("My warm greetings to you Nanna.");
+		console.log("I am blessed to have a father like you Nanna.");
+		
+
+	}
+	
